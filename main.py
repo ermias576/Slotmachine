80c81406ff1df5f2c45a8f76f549f5595c82d43b
ermias576 <ermiastegegn576@gmail.com>
2022-11-29 13:38:17 +0300

    def commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
@@ -9,6 +11,28 @@ def deposit():
                 print("Amount must be grater than 0.")
         else:
             print("Please enter a number.")
+    
     return amount
 
-deposit()
+def get_number_of_lines():
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be grater than 0.")
+        else:
+            print("Please enter a number.")
+    
+    return amount
+
+
+def main():
+    balance = deposit()
+
+
+
+main()
+
