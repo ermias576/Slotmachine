0e52ddd2d71d263be87f340d6ea456c0e29bffab
ermias576 <ermiastegegn576@gmail.com>
2022-11-29 13:46:39 +0300

    get lines number commit

--- main.py
+++ main.py
@@ -16,21 +16,23 @@ def deposit():
 
 def get_number_of_lines():
     while True:
-        amount = input("What would you like to deposit? $")
-        if amount.isdigit():
-            amount = int(amount)
-            if amount > 0:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1<= lines <= MAX_LINES:
                 break
             else:
-                print("Amount must be grater than 0.")
+                print("Enter a valid number of lines.")
         else:
-            print("Please enter a number.")
-    
-    return amount
+                print("Please enter a number.")
+
+    return lines
 
 
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    print(balance,lines)
 
 
 
